05191f536814eeab7c5f45b39afa17ff4e797936
@ <a@b.com>
2021-03-13 21:11:23 +0300

    3

--- owi.c
+++ owi.c
@@ -11,9 +11,9 @@
 
 #define	DELAY_US(x)	((F_CPU/1000000*(x))/8)
 
-volatile static uint8_t state;
-volatile static uint16_t tp;
-volatile static uint8_t byte;
+static uint8_t state;
+static uint16_t tp;
+static uint8_t byte;
 
 uint8_t owi_getState(void) {
 	return state;
@@ -25,11 +25,10 @@ void owi_init(void) {
 	PORTD &= ~0x04;
 	TCCR1A = 0;
 	TCCR1B = 1 << CS11;
-	MCUCR |= (1 << ISC01) | (1 << ISC00);
+	MCUCR |= (1 << ISC01);
 }
 
 void owi_reset(void) {
-	tp = 0;
 	DDRD |= 0x04;
 	OCR1A = TCNT1 + DELAY_US(500);
 	state = OWI_RESET0;
@@ -40,23 +39,21 @@ void owi_reset(void) {
 void owi_write_byte(uint8_t b) {
 	byte = b;
 	state = OWI_WRITE_BIT0;
-	uint16_t t0 = TCNT1;
 	DDRD |= 0x04;
 	if (byte & 1)
-		OCR1A = t0 + DELAY_US(5);
+		OCR1A = TCNT1 + DELAY_US(3);
 	else
-		OCR1A = t0 + DELAY_US(90);
-	OCR1B = t0 + DELAY_US(100);
+		OCR1A = TCNT1 + DELAY_US(90);
+	OCR1B = TCNT1 + DELAY_US(100);
 	TIFR = (1 << OCF1A) | (1 << OCF1B);
 	TIMSK |= (1 << OCIE1A) | (1 << OCIE1B);
 }
 
 void owi_read_byte(void) {
 	state = OWI_READ_BIT0;
-	uint16_t t0 = TCNT1;
 	DDRD |= 0x04;
-	OCR1A = t0 + DELAY_US(5);
-	OCR1B = t0 + DELAY_US(100);
+	OCR1A = TCNT1 + DELAY_US(3);
+	OCR1B = TCNT1 + DELAY_US(100);
 	TIFR = (1 << OCF1A) | (1 << OCF1B);
 	TIMSK |= (1 << OCIE1A) | (1 << OCIE1B);
 }
@@ -69,7 +66,7 @@ ISR(TIMER1_COMPA_vect) {
 	TIMSK &= ~(1 << OCIE1A);
 	if (state == OWI_RESET0) {
 		state = OWI_RESET_WF1;
-		MCUCR |= (1 << ISC01) | (1 << ISC00);
+		MCUCR |= (1 << ISC00);
 		GIFR = 1 << INTF0;
 		GICR |= 1 << INT0;
 		DDRD &= ~0x04;
@@ -80,17 +77,15 @@ ISR(TIMER1_COMPA_vect) {
 			state = OWI_PRESENCE;
 		else
 			state = OWI_IDLE;
-	} else if (state >= OWI_WRITE_BIT0 && state < OWI_WRITE_BIT0 + 8) {
+	} else if (state < OWI_WRITE_BIT0 + 8) {
 		DDRD &= ~0x04;
-	} else if (state >= OWI_READ_BIT0 && state < OWI_READ_BIT0 + 8) {
+	} else if (state < OWI_READ_BIT0 + 8) {
 		DDRD &= ~0x04;
-		PORTB |= 0x01;
 		state += 10;
 		OCR1A = TCNT1 + DELAY_US(3);
+		TIFR = (1 << OCF1A);
 		TIMSK |= (1 << OCIE1A);
-	} else if (state >= (OWI_READ_BIT0 + 10)
-			&& state < (OWI_READ_BIT0 + 10 + 8)) {
-		PORTB &= ~0x01;
+	} else if (state < (OWI_READ_BIT0 + 10 + 8)) {
 		byte >>= 1;
 		if (PIND & 0x04)
 			byte |= 0x80;
@@ -100,21 +95,20 @@ ISR(TIMER1_COMPA_vect) {
 
 ISR(TIMER1_COMPB_vect) {
 	TIMSK &= ~(1 << OCIE1B);
-	if (state >= OWI_WRITE_BIT0 && state < OWI_WRITE_BIT0 + 7) {
+	if (state < OWI_WRITE_BIT0 + 7) {
 		state++;
-		uint16_t t0 = TCNT1;
 		DDRD |= 0x04;
 		byte >>= 1;
 		if (byte & 1)
-			OCR1A = t0 + DELAY_US(5);
+			OCR1A = TCNT1 + DELAY_US(3);
 		else
-			OCR1A = t0 + DELAY_US(90);
-		OCR1B = t0 + DELAY_US(100);
+			OCR1A = TCNT1 + DELAY_US(90);
+		OCR1B = TCNT1 + DELAY_US(100);
 		TIFR = (1 << OCF1A) | (1 << OCF1B);
 		TIMSK |= (1 << OCIE1A) | (1 << OCIE1B);
 	} else if (state == OWI_WRITE_BIT0 + 7) {
 		state = OWI_WRITE_BYTE_OK;
-	} else if (state >= OWI_READ_BIT0 && state < OWI_READ_BIT0 + 7) {
+	} else if (state < OWI_READ_BIT0 + 7) {
 		state++;
 		DDRD |= 0x04;
 		OCR1A = TCNT1 + DELAY_US(3);
@@ -128,26 +122,21 @@ ISR(TIMER1_COMPB_vect) {
 
 ISR(INT0_vect) {
 	GICR &= ~(1 << INT0);
-	switch (state) {
-	case OWI_RESET_WF1:
+	if (state == OWI_RESET_WF1) {
 		state = OWI_WF_PRESENCE0;
 		OCR1A = TCNT1 + DELAY_US(500);
-		MCUCR |= (1 << ISC01);
 		MCUCR &= ~(1 << ISC00);
 		GIFR = (1 << INTF0);
 		TIFR = (1 << OCF1A);
 		GICR |= (1 << INT0);
 		TIMSK |= (1 << OCIE1A);
-		break;
-	case OWI_WF_PRESENCE0:
+	} else if (state == OWI_WF_PRESENCE0) {
 		state = OWI_WF_PRESENCE1;
 		tp = TCNT1;
-		MCUCR |= (1 << ISC01) | (1 << ISC00);
+		MCUCR |= (1 << ISC00);
 		GIFR = 1 << INTF0;
 		GICR |= 1 << INT0;
-		break;
-	case OWI_WF_PRESENCE1:
+	} else if (state == OWI_WF_PRESENCE1) {
 		tp = TCNT1 - tp;
-		break;
 	}
 }
